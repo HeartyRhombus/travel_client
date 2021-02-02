3c9c837736ddd73eba9f0d401a9b40dd4f6d389e
HeartyRhombus <kaylahmg@gmail.com>
2021-02-02 12:29:22 -0800

    stub out initial PlacesContainer

---
+++ src/containers/PlacesContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// connect to store
+// map thru places state to render place components
+class PlacesContainer extends Component {
+    render() {
+        return (
+            <div>
+                This is the places container
+            </div>
+        );
+    }
+}
+
+export default PlacesContainer;
