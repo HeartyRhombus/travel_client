30f6bbd906daa13737f4a98bde0a966e280e829b
HeartyRhombus <kaylahmg@gmail.com>
2021-02-02 10:16:30 -0800

    import applyMiddleware from redux

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 
