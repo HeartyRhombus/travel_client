c7b1262422d180ea98345c61d7731d4b712c06dc
HeartyRhombus <kaylahmg@gmail.com>
2021-02-02 12:37:49 -0800

    define const placesList from map thru places prop

--- src/containers/PlacesContainer.js
+++ src/containers/PlacesContainer.js
@@ -2,7 +2,15 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 // map thru places state to render place components
 class PlacesContainer extends Component {
+
     render() {
+        
+        const placesList = this.props.places.map( (p, i) => {
+            return (
+                <PlaceComponent key={i} place={p}/>
+            )
+        })
+
         return (
             <div>
                 This is the places container
