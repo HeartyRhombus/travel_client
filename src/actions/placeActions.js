ebfce187ce0eb92dab58469e1a10c286d3dc5196
HeartyRhombus <kaylahmg@gmail.com>
2021-02-02 13:25:57 -0800

    define fetch for places

--- src/actions/placeActions.js
+++ src/actions/placeActions.js
@@ -1,7 +1,11 @@
-export const getPlaces = () => {
+export const fetchPlaces = () => {
     return (dispatch) => {
-        dispatch({
-            type: 'LOAD_PLACES'
-        })
+        dispatch({ type: 'BEGIN_FETCHING_PLACES' })
+        fetch('http://localhost:3001/places')
+            .then(resp => resp.json())
+            .then(places => dispatch({
+                type: 'COMPLETED_FETCH_PLACES',
+                places
+            }))
     }
 }
