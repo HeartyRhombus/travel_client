5b2a1d73f0e795f8774578fe378de7b9c94313e1
HeartyRhombus <kaylahmg@gmail.com>
2021-02-02 12:40:20 -0800

    stub out initial PlaceComponent file

---
+++ src/components/PlaceComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PlaceComponent = (props) => {
+    return (
+        <div>
+            Horray you've reached a place component!
+        </div>
+    )
+}
+
+export default PlaceComponent
