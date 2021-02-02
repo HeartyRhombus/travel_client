b21d51e4e0d069ee368708b41dc447f2a9dbb63a
HeartyRhombus <kaylahmg@gmail.com>
2021-02-02 11:03:26 -0800

    define  const rootReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -2,3 +2,9 @@ import { combineReducers } from 'redux'
 import eventReducer from './eventReducer'
 import placeReducer from './placeReducer'
 
+const rootReducer = combineReducers({
+    placeReducer,
+    eventReducer
+})
+
+export default rootReducer
