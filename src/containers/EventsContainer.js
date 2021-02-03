1b4daa535c73f1c362484ca3d5c542823f9c20c4
HeartyRhombus <kaylahmg@gmail.com>
2021-02-03 09:06:40 -0800

    stub out initial events container file

---
+++ src/containers/EventsContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class EventsContainer extends Component {
+    render() {
+        return (
+            <div>
+                You have reached the events container!
+            </div>
+        );
+    }
+}
+
+export default EventsContainer;
