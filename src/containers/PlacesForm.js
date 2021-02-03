4aca02b2ff28206aaab98c9eeccb831555934778
HeartyRhombus <kaylahmg@gmail.com>
2021-02-02 16:50:50 -0800

    add handleChange & handleToggle onChange event methods

--- src/containers/PlacesForm.js
+++ src/containers/PlacesForm.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
 
 class PlacesForm extends Component {
-    
+
+    state = {
+        city: "", 
+        country: "",
+        visited: false
+    }
+
+    handleChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleToggle = event => {
+        console.log(event.target.checked)
+        this.setState({
+            visited: event.target.checked
+        })
+    }
+
     render() {
         return (
             <form>
-                <input name="city" type="text" placeholder="City" />
-                <input name="country" type="text" placeholder="Country" />
+                <input name="city" type="text" placeholder="City" onChange={this.handleChange} />
+                <input name="country" type="text" placeholder="Country" onChange={this.handleChange} />
                 <br/>
                 <label>
                     Been There?
-                    <input name="visited" type="checkbox" placeholder="Yes" />
+                    <input name="visited" type="checkbox" onChange={this.handleToggle}/>
                 </label>
                 <br/>
                 <input type="submit" />
