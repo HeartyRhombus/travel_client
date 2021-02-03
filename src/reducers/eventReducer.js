8f7b5b3a3bf98e52f8be3283ea3b8a03b37ac53c
HeartyRhombus <kaylahmg@gmail.com>
2021-02-03 09:19:30 -0800

    add fetch all events cases

--- src/reducers/eventReducer.js
+++ src/reducers/eventReducer.js
@@ -1,5 +1,20 @@
 const eventReducer = (state = {events: [], loading: false}, action) => {
     switch(action.type){
+
+        case 'BEGIN_FETCHING_ALL_EVENTS':
+            return {
+                ...state,
+                events: [...state.events],
+                loading: true
+            }
+
+        case 'COMPLETED_FETCH_ALL_EVENTS':
+            return {
+                ...state,
+                events: action.events,
+                loading: false
+            }
+
         default:
             return state
     }
