915784bc0f2a23c8118740ea5ffeea38b38f7e3b
HeartyRhombus <kaylahmg@gmail.com>
2021-02-03 09:11:24 -0800

    connect component to the store

--- src/containers/EventsContainer.js
+++ src/containers/EventsContainer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 
 class EventsContainer extends Component {
     render() {
@@ -10,4 +11,10 @@ class EventsContainer extends Component {
     }
 }
 
-export default EventsContainer;
+const mapStateToProps = state => {
+    return {
+        events: state.eventReducer.events
+    }
+}
+
+export default connect(mapStateToProps)(EventsContainer);
