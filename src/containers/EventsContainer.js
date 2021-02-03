f75e4ebf101223b8c2eb6772f23371ae579a381e
HeartyRhombus <kaylahmg@gmail.com>
2021-02-03 09:52:04 -0800

    add fetchAllEvents method

--- src/containers/EventsContainer.js
+++ src/containers/EventsContainer.js
@@ -1,20 +1,26 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import EventComponent from '../components/EventComponent'
+import { fetchAllEvents } from '../actions/eventActions'
 
 class EventsContainer extends Component {
+
+    componentDidMount(){
+        this.props.fetchAllEvents()
+    }
 
     render() {
 
         const eventsList = this.props.events.map( (e, i) => {
             return (
                 <EventComponent key={i} event={e} />
+                
             )
         })
 
         return (
             <div>
-                You have reached the events container!
+                {/* You have reached the events container! */}
                 {eventsList}
             </div>
         );
@@ -27,4 +33,4 @@ const mapStateToProps = state => {
     }
 }
 
-export default connect(mapStateToProps)(EventsContainer);
+export default connect(mapStateToProps, { fetchAllEvents })(EventsContainer);
