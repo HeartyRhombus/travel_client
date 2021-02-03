6d36ef8e94b09f3821a27183f5b44037c331eadb
HeartyRhombus <kaylahmg@gmail.com>
2021-02-03 09:14:46 -0800

    render initial event components

--- src/containers/EventsContainer.js
+++ src/containers/EventsContainer.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
+import EventComponent from '../components/EventComponent'
 
 class EventsContainer extends Component {
+
     render() {
+
+        const eventsList = this.props.events.map( (e, i) => {
+            return (
+                <EventComponent key={i} event={e} />
+            )
+        })
+
         return (
             <div>
                 You have reached the events container!
+                {eventsList}
             </div>
         );
     }
