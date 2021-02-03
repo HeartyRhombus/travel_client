60faeee49ccdd94e73cab8ede97be6361bdad6f4
HeartyRhombus <kaylahmg@gmail.com>
2021-02-03 09:11:08 -0800

    add initial event reducer method

---
+++ src/reducers/eventReducer.js
@@ -0,0 +1,8 @@
+const eventReducer = (state = {events: [], loading: false}, action) => {
+    switch(action.type){
+        default:
+            return state
+    }
+}
+
+export default eventReducer
