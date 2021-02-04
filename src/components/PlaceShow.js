ed876066a1289592ea15162b1bc0b68cb00612f1
HeartyRhombus <kaylahmg@gmail.com>
2021-02-04 13:00:30 -0800

    update to render any newly added events

--- src/components/PlaceShow.js
+++ src/components/PlaceShow.js
@@ -1,16 +1,18 @@
 import React, {Component} from 'react'
 import { connect } from 'react-redux'
 import NavBar from '../components/NavBar'
+import EventsForm from '../containers/EventsForm'
 
 class PlaceShow extends Component{
+
     render(){
         const id = this.props.match.params.id
+        const selectedEvents = this.props.events.filter(event => event.place_id == id)
         const selectedPlace = this.props.places.find( place => place.id == id )
 
-        const thingsToDo = selectedPlace.events.map( event => {
+        const thingsToDo = selectedEvents.map( event => {
             return (
                 <div>
-                    Things To Do In {selectedPlace.city}:
                     <li>
                         {event.name}
                     </li>
@@ -27,7 +29,13 @@ class PlaceShow extends Component{
 
                 <h3>{selectedPlace.city}, {selectedPlace.country}</h3>
                 <hr/>
+                Things To Do In {selectedPlace.city}:
                 {thingsToDo}
+                {console.log(selectedEvents)}
+                <hr/>
+                <div className='addEventForm'>
+                    <EventsForm placeId={id}/>
+                </div>
             </div>
         )
     }
@@ -35,7 +43,8 @@ class PlaceShow extends Component{
 
 const mapStateToProps = state => {
     return {
-        places: state.placeReducer.places
+        places: state.placeReducer.places,
+        events: state.eventReducer.events
     }
 }
 
