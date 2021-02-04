5fd11748e5340bf58657608b54558561b94bb7bd
HeartyRhombus <kaylahmg@gmail.com>
2021-02-04 11:26:55 -0800

    add fetchPlaces and fetchAllEvents methods

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react';
 import './App.css'
+import { connect } from 'react-redux'
+import NavBar from './components/NavBar'
+import { fetchPlaces } from './actions/placeActions'
+import { fetchAllEvents } from './actions/eventActions'
 import PlacesContainer from './containers/PlacesContainer'
 
 class App extends Component {
+
+  componentDidMount(){
+    this.props.fetchPlaces()
+    this.props.fetchAllEvents()
+  }
+
   render() {
     return (
       <div className="App">
+        <div className='navbar'>
+          <NavBar />
+        </div>
         <h1>My Travel Bucket List!</h1>
-        <PlacesContainer />
+        <PlacesContainer/>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  // console.log(state.placeReducer.places)
+  return {
+    ...state,
+      places: state.placeReducer.places,
+      events: state.eventReducer.events
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  { fetchPlaces, fetchAllEvents }
+  )(App);
