aca100e7c7449f2979359c6915b26123b4b392c4
HeartyRhombus <kaylahmg@gmail.com>
2021-02-04 13:50:08 -0800

    add button styling to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,6 +6,7 @@ export default function NavBar() {
     return (
         <Navbar bg="light">
             <Button variant="outline-secondary"><Link to="/">Home</Link></Button>
+            <Button variant="outline-secondary"><Link to="/places">Destinations</Link></Button>
             <Button variant="outline-secondary"><Link to="/events">Things To Do</Link></Button>
         </Navbar>
     )
