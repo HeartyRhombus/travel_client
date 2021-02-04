18d202afcbccd8ae94ae9a07f42040c521f84dfb
HeartyRhombus <kaylahmg@gmail.com>
2021-02-04 13:50:31 -0800

    import card from react-bootstrap and create cards

--- src/components/PlaceComponent.js
+++ src/components/PlaceComponent.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import { Card } from 'react-bootstrap'
 
 
 const PlaceComponent = ( props ) => {
 
     return (
-        <div>
-            <Link to={`/places/${props.place.id}`} >
-            {props.place.city}, {props.place.country}, {props.place.visited ? "Yes, I've been here!" : "Not Visted Yet"}
-            </Link>
-        </div>
+        <Card style={{ width: '18rem' }}>
+            <Card.Body>
+                <Card.Title>
+                    <Link to={`/places/${props.place.id}`} >
+                        {props.place.city}, {props.place.country}
+                    </Link>
+                </Card.Title>
+                <Card.Text>
+                {props.place.visited ? "Yes, I've been here!" : "Not Visted Yet"}
+                </Card.Text>
+            </Card.Body>
+            {/* {props.place.city}, {props.place.country},  */}
+        </Card>
     )
 }
 
