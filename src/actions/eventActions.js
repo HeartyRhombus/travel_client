27033d7561dc0d09c6025c43d85723ddf4c385ee
HeartyRhombus <kaylahmg@gmail.com>
2021-02-04 11:26:17 -0800

    add addEvent post action

--- src/actions/eventActions.js
+++ src/actions/eventActions.js
@@ -8,3 +8,19 @@ export const fetchAllEvents = () => {
             }))
     }
 }
+
+export const addEvent = (event) => {
+    return (dispatch) => {
+        dispatch({type: 'ADD_EVENT'})
+        fetch('http://localhost:3001/events', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify(event)
+        })
+            .then(resp => resp.json())
+            .then(event => dispatch({type: 'EVENT_ADDED', event}))
+    }
+}
