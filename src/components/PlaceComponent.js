b7bffa57b4ac8ba7d877cf5909dd997017a20c87
HeartyRhombus <kaylahmg@gmail.com>
2021-02-04 11:25:32 -0800

    remove unneeded reference to place.id

--- src/components/PlaceComponent.js
+++ src/components/PlaceComponent.js
@@ -7,7 +7,6 @@ const PlaceComponent = ( props ) => {
     return (
         <div>
             <Link to={`/places/${props.place.id}`} >
-            {props.place.id}.
             {props.place.city}, {props.place.country}, {props.place.visited ? "Yes, I've been here!" : "Not Visted Yet"}
             </Link>
         </div>
