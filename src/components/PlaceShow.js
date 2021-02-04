4aa8b3e76dcf051ed3f9d21650fd07d8289985ab
HeartyRhombus <kaylahmg@gmail.com>
2021-02-04 11:25:56 -0800

    redefine placeShow component

--- src/components/PlaceShow.js
+++ src/components/PlaceShow.js
@@ -1,17 +1,42 @@
-import React from 'react'
-import {useParams} from 'react-router-dom'
-import _ from 'lodash'
+import React, {Component} from 'react'
+import { connect } from 'react-redux'
+import NavBar from '../components/NavBar'
 
-const PlaceShow = ( {place} ) => {
-    const {id} = useParams()
-    const selectedPlace = _.filter(place, {id: `${id}`})
-    console.log(selectedPlace)
+class PlaceShow extends Component{
+    render(){
+        const id = this.props.match.params.id
+        const selectedPlace = this.props.places.find( place => place.id == id )
 
-    return (
-        <div>
-            {selectedPlace.name}
-        </div>
-    )
+        const thingsToDo = selectedPlace.events.map( event => {
+            return (
+                <div>
+                    Things To Do In {selectedPlace.city}:
+                    <li>
+                        {event.name}
+                    </li>
+                </div>
+            )
+        })
+
+
+        return (
+            <div>
+                <div className="navbar">
+                    <NavBar />
+                </div>
+
+                <h3>{selectedPlace.city}, {selectedPlace.country}</h3>
+                <hr/>
+                {thingsToDo}
+            </div>
+        )
+    }
 }
 
-export default PlaceShow
+const mapStateToProps = state => {
+    return {
+        places: state.placeReducer.places
+    }
+}
+
+export default connect(mapStateToProps)(PlaceShow)
