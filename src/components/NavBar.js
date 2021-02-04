8d0cb718131876714acfbcbab879cb7303633c3b
HeartyRhombus <kaylahmg@gmail.com>
2021-02-04 13:31:24 -0800

    add styling to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Navbar, Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 export default function NavBar() {
     return (
-        <div>
-            <Link to="/">Home</Link>
-            <Link to="/events">Things To Do</Link>
-        </div>
+        <Navbar bg="light">
+            <Button variant="outline-secondary"><Link to="/">Home</Link></Button>
+            <Button variant="outline-secondary"><Link to="/events">Things To Do</Link></Button>
+        </Navbar>
     )
 }
