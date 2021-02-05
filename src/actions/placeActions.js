a8972f5e39d6c37c153823ca814f757b7bf80694
HeartyRhombus <kaylahmg@gmail.com>
2021-02-05 10:21:37 -0800

    add updatePlace action

--- src/actions/placeActions.js
+++ src/actions/placeActions.js
@@ -24,3 +24,19 @@ export const addPlace = (place) => {
             .then(place => dispatch({ type: 'PLACE_ADDED', place }))
     }
 }
+
+export const updatePlace = (place) => {
+    return (dispatch) => {
+        dispatch({ type: 'UPDATE_PLACE' })
+        fetch(`http://localhost:3001/places/${place.id}`, {
+            method: 'PATCH',
+            header: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify(place)
+        })
+            .then(resp => resp.json())
+            .then(place => dispatch({ type: 'PLACE_UPDATED', place}))
+    }
+}
